055a041230dd3b7f2a7ec12a744b62d4f416e4b9
MC333 <manuel.alejandrocastro1990@gmail.com>
2019-05-06 22:27:03 -0400

    Initial commit

---
+++ app/src/main/java/castro/inc/currencyconverter/MainActivity.java
@@ -0,0 +1,55 @@
+package castro.inc.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void poundFunction(View convertView){
+
+        EditText currencytText = findViewById(R.id.currencyText);
+        Log.i("Amount", currencytText.getText().toString());
+
+        Double currencyTextDouble = Double.parseDouble(currencytText.getText().toString());
+        Double poundAmount = .75*(currencyTextDouble);
+        Toast.makeText(MainActivity.this, "£" + String.format("%.2f", poundAmount), Toast.LENGTH_LONG).show();
+    }
+
+    public void yenFunction(View convertView){
+
+        EditText currencytText = findViewById(R.id.currencyText);
+        Log.i("Amount", currencytText.getText().toString());
+
+        Double currencyTextDouble = Double.parseDouble(currencytText.getText().toString());
+        Double yenAmount = 111.66*(currencyTextDouble);
+        Toast.makeText(MainActivity.this, "¥" + String.format("%.2f", yenAmount), Toast.LENGTH_LONG).show();
+    }
+    public void pesoFunction(View convertView){
+
+        EditText currencytText = findViewById(R.id.currencyText);
+        Log.i("Amount", currencytText.getText().toString());
+
+        Double currencyTextDouble = Double.parseDouble(currencytText.getText().toString());
+        Double pesoAmount = 19.02*(currencyTextDouble);
+        Toast.makeText(MainActivity.this, "$" + String.format("%.2f", pesoAmount), Toast.LENGTH_LONG).show();
+    }
+    public void euroFunction(View convertView){
+
+        EditText currencytText = findViewById(R.id.currencyText);
+        Log.i("Amount", currencytText.getText().toString());
+
+        Double currencyTextDouble = Double.parseDouble(currencytText.getText().toString());
+        Double euroAmount = .90*(currencyTextDouble);
+        Toast.makeText(MainActivity.this, "€" + String.format("%.2f", euroAmount), Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
